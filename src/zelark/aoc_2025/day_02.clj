d376d4965dc6bf42ebd8768a389c3d3d28e966f1
Aleksandr Zhuravlev <kvark.lark@gmail.com>
2025-12-02 08:27:00 +0300

    [2025 Day 02] Solve part 1 and 2

---
+++ src/zelark/aoc_2025/day_02.clj
@@ -0,0 +1,25 @@
+(ns zelark.aoc-2025.day-02
+  (:require [zelark.aoc.core :as aoc]))
+
+;; --- Day 2: Gift Shop ---
+;; https://adventofcode.com/2025/day/2
+
+(def input (aoc/get-input 2025 2))
+
+(defn parse-input [input]
+  (->> (re-seq #"(\d+)-(\d+)" input)
+       (mapv (fn [[_ start end]]
+               [(parse-long start)
+                (parse-long end)]))))
+
+(defn solve [input invalid-id?]
+  (aoc/sum (for [[start end] (parse-input input)
+                 num (range start (inc end))
+                 :when (invalid-id? num)]
+             num)))
+
+;; part 1 (414.583925 msecs)
+(time (solve input #(boolean (re-matches #"^(.+)\1$" (str %))))) ; 28846518423
+
+;; part 2 (412.718297 msecs)
+(time (solve input #(boolean (re-matches #"^(.+)\1+$" (str %))))) ; 31578210022
